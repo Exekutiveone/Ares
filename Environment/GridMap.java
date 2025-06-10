177431cc29721ef445deee7ba1af37c3e066d4f7
Exekutiveone <110990173+Exekutiveone@users.noreply.github.com>
2025-06-10 16:30:18 +0200

    Improve target placement and UI

--- Environment/GridMap.java
+++ Environment/GridMap.java
@@ -2,6 +2,7 @@ package Environment;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class GridMap {
     public int width;
@@ -10,6 +11,7 @@ public class GridMap {
     public List<Asset> assets;
     public List<Coord> obstacles;
     public List<Coord> targets;
+    private final Random rand = new Random();
 
     public GridMap(int width, int height) {
         this.width = width;
@@ -40,6 +42,22 @@ public class GridMap {
             targets.add(c);
             gridArray[c.y][c.x] = 2;
         }
+    }
+
+    /**
+     * Place a target at a random free coordinate on the map.
+     * @return the coordinate of the placed target or null if no free spot was found
+     */
+    public Coord placeRandomTarget() {
+        int attempts = width * height * 2;
+        while (attempts-- > 0) {
+            Coord c = new Coord(rand.nextInt(width), rand.nextInt(height));
+            if (isFree(c)) {
+                addTarget(c);
+                return c;
+            }
+        }
+        return null;
     }
 
     public void addAsset(Asset asset, Coord c) {
