fcf9f2557e055e9e542dd469a8afe3da320543e9
Exekutiveone <110990173+Exekutiveone@users.noreply.github.com>
2025-06-10 13:32:27 +0200

    Add DB save/load and resize features to SimulatedAsset

--- SimulateAsset/map.js
+++ SimulateAsset/map.js
@@ -1,13 +1,29 @@
+import { Obstacle } from './obstacle.js'
+
 export class GameMap {
-  constructor(width, height, margin, cellSize = 40) {
-    this.width = width
-    this.height = height
-    this.margin = margin
+  constructor(cols, rows, cellSize = 40, margin = 0) {
+    this.cols = cols
+    this.rows = rows
     this.cellSize = cellSize
+    this.margin = margin
+    this.obstacles = []
+    this.task = null
+  }
+
+  get width() { return this.cols * this.cellSize }
+  get height() { return this.rows * this.cellSize }
+
+  isWithinBounds(x, y, objWidth = 0, objHeight = 0) {
+    return (
+      x >= this.margin &&
+      y >= this.margin &&
+      x + objWidth <= this.width - this.margin &&
+      y + objHeight <= this.height - this.margin
+    )
   }
 
   drawGrid(ctx) {
-    ctx.strokeStyle = '#000' // alternativ 'red' für Sichtbarkeit
+    ctx.strokeStyle = '#000'
     ctx.lineWidth = 1
     for (let x = 0; x <= this.width; x += this.cellSize) {
       ctx.beginPath()
@@ -29,5 +45,25 @@ export class GameMap {
     ctx.fillRect(0, this.height - this.margin, this.width, this.margin)
     ctx.fillRect(0, 0, this.margin, this.height)
     ctx.fillRect(this.width - this.margin, 0, this.margin, this.height)
+  }
+
+  toJSON() {
+    return {
+      cols: this.cols,
+      rows: this.rows,
+      cellSize: this.cellSize,
+      margin: this.margin,
+      obstacles: this.obstacles.map(o => ({ x: o.x, y: o.y, size: o.size })),
+      task: this.task
+    }
+  }
+
+  static fromJSON(obj) {
+    const gm = new GameMap(obj.cols, obj.rows, obj.cellSize, obj.margin)
+    if (obj.obstacles) {
+      gm.obstacles = obj.obstacles.map(o => new Obstacle(o.x, o.y, o.size))
+    }
+    gm.task = obj.task || null
+    return gm
   }
 }
